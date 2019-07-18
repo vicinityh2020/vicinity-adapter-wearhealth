11d27a9233bea26a0352bba19089a8bb28de4214
Jubayed <$hahNewaz1Jub$hant1>
2019-07-18 23:28:07 +0200

    Copyright and license added

--- src/main/java/com/wearhealth/adapter/template/Application.java
+++ src/main/java/com/wearhealth/adapter/template/Application.java
@@ -1,8 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+Copyright © 2019 Wearhealth. All rights reserved.
+
+This file is part of vicinity-adapter-wearhealth.
+
+vicinity-adapter-wearhealth is free software: you can redistribute it and/or modify it under the terms of GNU General Public License v3.0.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT ANY WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT, IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+See README file for the full disclaimer information and LICENSE file for full license information in the project root.
+*/
 package com.wearhealth.adapter.template;
 
 import com.wearhealth.adapter.template.pojos.Response;
